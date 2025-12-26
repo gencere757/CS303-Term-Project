064f6034dbc09d2c0b7faa40bf6e08ac5344d194
gencere757 <arda.gencer@sabanciuniv.edu>
2025-12-26 19:49:32 +0300

    Add files via upload

---
+++ mastermind.v
@@ -0,0 +1,21 @@
+module mastermind(
+
+    input clk,
+    input rst,
+    
+    input enterA,
+    input enterB,
+    input [2:0] letterIn,			
+   
+    output reg [7:0] LEDX,
+    output reg [6:0] SSD3,
+    output reg [6:0] SSD2,
+    output reg [6:0] SSD1,
+    output reg [6:0] SSD0 
+    
+    );
+    
+   
+    
+    
+endmodule
